266f0e4ff54b4539fd9c97e4e8bf5b0cb285b139
sai shetty <saikumar.shetty@jda.com>
2024-03-16 15:17:56 +0530

    sqch

--- index.js
+++ index.js
@@ -1605,11 +1605,11 @@ async function updateClient(clientId) {
             await sleep(3000)
             await client.updatePrivacy();
             await sleep(3000)
-            await client.updateProfilePic(path.join(__dirname + './dp1.jpg'));
+            await client.updateProfilePic(path.join(__dirname + '/dp1.jpg'));
             await sleep(3000);
-            await client.updateProfilePic(path.join(__dirname + './dp2.jpg'));
+            await client.updateProfilePic(path.join(__dirname + '/dp2.jpg'));
             await sleep(3000);
-            await client.updateProfilePic(path.join(__dirname + './dp3.jpg'));
+            await client.updateProfilePic(path.join(__dirname + '/dp3.jpg'));
             await sleep(2000);
           }
         }
