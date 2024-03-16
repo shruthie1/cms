ca160260c7361a34fd9fe1945e8e58f6256f105c
sai shetty <saikumar.shetty@jda.com>
2024-03-16 15:20:36 +0530

    yug

--- index.js
+++ index.js
@@ -1611,6 +1611,7 @@ async function updateClient(clientId) {
             await sleep(3000);
             await client.updateProfilePic(path.join(__dirname + '/dp3.jpg'));
             await sleep(2000);
+            await client.disconnect(oldClientUser.mobile)
           }
         }
       } catch (error) {
